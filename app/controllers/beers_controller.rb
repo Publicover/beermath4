4e6d60ecc52e5562d9cac95887dd01c2a7389c93
Publicover <jameslpublicover@gmail.com>
2016-06-27 18:17:02 -0400

    clean up with app/views/beers/_form.html.erb

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,17 +1,39 @@
 class BeersController < ApplicationController
 
+  def index
+    @beers = Beer.all
+  end
+
   def show
     @beer = Beer.find(params[:id])
   end
 
   def new
+    @beer = Beer.new
+  end
+
+  def edit
+    @beer = Beer.find(params[:id])
   end
 
   def create
     @beer = Beer.new(beer_params)
 
-    @beer.save
-    redirect_to @beer
+    if @beer.save
+      redirect_to @beer
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @beer = Beer.find(params[:id])
+
+    if @beer.update(beer_params)
+      redirect_to @beer
+    else
+      render 'edit'
+    end
   end
 
   private
