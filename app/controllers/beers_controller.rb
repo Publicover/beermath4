196c6a3e268b90db1baa7b01ceb43daf2f5d3f62
Publicover <jameslpublicover@gmail.com>
2016-06-27 16:48:32 -0400

    put up model, forms and controller for beer. can now save to db.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,2 +1,22 @@
 class BeersController < ApplicationController
+
+  def show
+    @beer = Beer.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+
+    @beer.save
+    redirect_to @beer
+  end
+
+  private
+    def beer_params
+      params.require(:beer).permit(:name, :brewer, :price, :ounce, :calorie, :rating)
+    end
+
 end
