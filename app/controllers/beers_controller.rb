4a3dede1def7b4821ea537a29815f6a1594af0aa
Publicover <jameslpublicover@gmail.com>
2016-06-30 16:06:58 -0400

    remove if conditional in beers/show

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,5 @@
 class BeersController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:index, :show, :delete]
   # before_filter :authenticate_user!, except: :show
   helper_method :sort_column, :sort_direction
 
