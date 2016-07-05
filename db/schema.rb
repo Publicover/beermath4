bcaca58e036124b03685dcfa16fd3d5a24c5c0d5
Publicover <jameslpublicover@gmail.com>
2016-07-05 10:16:03 -0400

    redo ABV into database. next is adding private methods that sort calculations into database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629205316) do
+ActiveRecord::Schema.define(version: 20160705140653) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160629205316) do
     t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "abv"
   end
 
   create_table "reviews", force: :cascade do |t|
