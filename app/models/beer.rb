e0834a2a242ee5d8082b86612c32eac3f70163ee
Publicover <jameslpublicover@gmail.com>
2016-07-05 08:12:33 -0400

    remove previous attempt

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,15 @@
 class Beer < ActiveRecord::Base
+  before_save
   has_many :reviews, dependent: :destroy
   validates :name, presence: true,
                    length: { minimum: 2 }
+
+  def get_abv_per_cal
+    self.get_abv_per_cal = calorie.to_f / abv
+    # (self.get_abv_per_cal).save
+  end
+
+
 end
 
 #comment here to check moving back a version in git
