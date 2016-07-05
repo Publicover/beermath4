5e476802772ac2dd3f16ea06026005f63d471a48
Publicover <jameslpublicover@gmail.com>
2016-07-05 12:12:35 -0400

    put custom signup routes back in at top of routes and added signup link to dashboards/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new"
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" 
+  end
+
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
+
   get 'dashboards/index'
 
   authenticate :user do
