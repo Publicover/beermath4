0750192d520915ee1fd1ad29f1f9bdd42947e2db
Publicover <jameslpublicover@gmail.com>
2016-07-05 08:58:10 -0400

    add abv_per_cal and abv_per_price methods in beer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705122054) do
+ActiveRecord::Schema.define(version: 20160705125545) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
@@ -20,10 +20,12 @@ ActiveRecord::Schema.define(version: 20160705122054) do
     t.integer  "ounce"
     t.integer  "calorie"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "abv"
     t.integer  "sum"
+    t.float    "abv_per_cal"
+    t.float    "abv_per_price"
   end
 
   create_table "reviews", force: :cascade do |t|
