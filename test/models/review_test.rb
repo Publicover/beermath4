2e7681f0b09e8208f9f24d8ec741bb3349c89fc3
Publicover <jameslpublicover@gmail.com>
2016-07-06 17:58:31 -0400

    review unit test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -7,4 +7,19 @@ class ReviewTest < ActiveSupport::TestCase
     assert Review
   end
 
+  test "review returns a reviewer" do
+    review = reviews(:one)
+    assert_equal "JIMTEST", review.reviewer
+  end
+
+  test "review returns a body" do
+    review = reviews(:two)
+    assert_equal "it's good it's good it's good", review.body
+  end
+
+  test "review returns a beer_id" do
+    review = reviews(:one)
+    assert_equal 1, review.beer_id
+  end
+
 end
